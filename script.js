322297a876a42070dca52428fa3f3be0d4e316d5
Mayur Panchal <mayurpanchal002@gmail.com>
2020-10-02 18:52:34 +0530

    added dark theme support

--- script.js
+++ script.js
@@ -1,26 +1,43 @@
-const input = document.getElementById("input");
-const display = document.querySelector(".display");
-const form = document.querySelector("form");
-const gif = document.querySelector(".gif");
-const button = document.querySelector("button");
-const loader = document.querySelector(".loader");
-const link = document.querySelector(".link");
+const input = document.getElementById('input');
+const display = document.querySelector('.display');
+const form = document.querySelector('form');
+const gif = document.querySelector('.gif');
+const button = document.querySelector('button');
+const loader = document.querySelector('.loader');
+const link = document.querySelector('.link');
+const theme = document.querySelector('#theme');
+const themeIcon = document.querySelector('#theme-icon');
 
-form.addEventListener("submit", (e) => {
-  e.preventDefault();
-  const tag = input.value;
-  const gifURL = `https://api.giphy.com/v1/gifs/random?api_key=zwtSi8hA6IZtjFDBU9ZgoxOCeh90beEl&tag=${tag}&rating=r`;
-  loader.style.display = "block";
+let isDark = false;
 
-  fetch(gifURL)
-    .then((datas) => datas.json())
-    .then((response) => {
-      link.innerHTML = response.data.fixed_height_downsampled_url;
-      gif.src = response.data.fixed_height_downsampled_url;
+form.addEventListener('submit', (e) => {
+	e.preventDefault();
+	const tag = input.value;
+	const gifURL = `https://api.giphy.com/v1/gifs/random?api_key=zwtSi8hA6IZtjFDBU9ZgoxOCeh90beEl&tag=${tag}&rating=r`;
+	loader.style.display = 'block';
 
-      form.reset();
-      loader.style.display = "none";
-      link.style.display = "block";
-    })
-    .catch((err) => console.log(err));
+	fetch(gifURL)
+		.then((datas) => datas.json())
+		.then((response) => {
+			link.innerHTML = response.data.fixed_height_downsampled_url;
+			gif.src = response.data.fixed_height_downsampled_url;
+
+			form.reset();
+			loader.style.display = 'none';
+			link.style.display = 'block';
+		})
+		.catch((err) => console.log(err));
+});
+
+theme.addEventListener('click', () => {
+	if (isDark) {
+		document.querySelector('body').classList.remove('dark');
+		document.querySelector('body').classList.add('light');
+		themeIcon.setAttribute('src', './icon/sun.png');
+	} else {
+		document.querySelector('body').classList.remove('light');
+		document.querySelector('body').classList.add('dark');
+		themeIcon.setAttribute('src', './icon/moon.png');
+	}
+	isDark = !isDark;
 });
